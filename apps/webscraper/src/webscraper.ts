1fb3777b291b8c24e3630f2b6197a190b51bad4b
Fanny Reslow <reslowf@gmail.com>
2022-07-11 21:17:08 +0200

    logging out content on subpage

--- apps/webscraper/src/webscraper.ts
+++ apps/webscraper/src/webscraper.ts
@@ -4,7 +4,8 @@ import type { List } from '@prisma/client';
 
 export default async function Webscraper() {
   async function setUpBrowser() {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({ headless: false });
+
     const page = await browser.newPage();
     await page.goto(
       'https://arbetsformedlingen.se/platsbanken/annonser?q=front%20end',
@@ -28,63 +29,68 @@ export default async function Webscraper() {
       }
     });
 
-    if (data !== undefined) {
+    const newList = data?.forEach(async (element) => {
       const newPage = await browser.newPage();
-      console.log(data[0].url);
-      await newPage.goto(`https://arbetsformedlingen.se${data[0].url}`);
-      await newPage.waitForSelector('.job-description');
-      console.log('tjena');
 
-      const hej = {
-        title: 'a',
-        company: 'b',
-        position: 'b',
-        url: 'c',
-        desc: 'd',
-      };
-      return hej;
-    }
+      await newPage.goto(`https://arbetsformedlingen.se${element.url}`);
+      console.log(`https://arbetsformedlingen.se${element.url}`);
+      await newPage.waitForSelector('.job-info');
 
-    async function createList() {
-      const title = 'arbetsförmedlingen';
-      const theList = await prisma.list.create({
+      const data = await newPage.evaluate(() => {
+        console.log('Eval');
+        const item = document.querySelector('.job-info')?.innerHTML;
+        console.log(item);
+
+        console.log('-----');
+        console.log({ item });
+        console.log('-----');
+        return item;
+      });
+      console.log(data);
+
+      return data;
+    });
+    return newList;
+  }
+
+  async function createList() {
+    const title = 'arbetsförmedlingen';
+    const theList = await prisma.list.create({
+      data: {
+        title,
+      },
+    });
+    return theList;
+  }
+
+  async function saveDataToList(
+    data: {
+      title: string;
+      company: string;
+      position: string;
+      url: string;
+      desc: string;
+    }[],
+    list: List,
+  ) {
+    data.forEach(async (item) => {
+      await prisma.listItem.createMany({
         data: {
-          title,
+          title: item.title,
+          company: item.company,
+          position: item.position,
+          url: item.url,
+          listId: list.id,
+          desc: item.desc,
         },
       });
-      return theList;
-    }
-
-    async function saveDataToList(
-      data: {
-        title: string;
-        company: string;
-        position: string;
-        url: string;
-        desc: string;
-      }[],
-      list: List,
-    ) {
-      data.forEach(async (item) => {
-        await prisma.listItem.createMany({
-          data: {
-            title: item.title,
-            company: item.company,
-            position: item.position,
-            url: item.url,
-            listId: list.id,
-            desc: item.desc,
-          },
-        });
-      });
-    }
-
-    const list = await createList();
-    const dat = await setUpBrowser();
-
-    if (dat) {
-      console.log(dat);
-      saveDataToList(dat, list);
-    } else return;
+    });
   }
+
+  const list = await createList();
+  const data = await setUpBrowser();
+
+  if (data !== undefined) {
+    saveDataToList(data, list);
+  } else return;
 }
