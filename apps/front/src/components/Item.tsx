1e52020a0611c6152dd4167a4cac805a50f41b10
Fanny Reslow <reslowf@gmail.com>
2022-07-13 14:22:35 +0200

    coonverted strings to html

--- apps/front/src/components/Item.tsx
+++ apps/front/src/components/Item.tsx
@@ -1,16 +1,22 @@
 import type { ListItem } from "@prisma/client";
 import "./item.css";
+import { Markup } from "interweave";
+import Parser from "html-react-parser";
 
 type Props = {
   listItem: ListItem;
 };
 
 export default function Item({ listItem }: Props) {
+  const description = listItem.desc;
+
   return (
     <div className="bg">
       <p>{listItem.position}</p>
       <p>{listItem.company}</p>
       <a href={`http://www.arbetsformedlingen.se${listItem.url}`}>to ad</a>
+      <p>{Parser(description)}</p>
+      {/* <Markup content={description} /> */}
     </div>
   );
 }
