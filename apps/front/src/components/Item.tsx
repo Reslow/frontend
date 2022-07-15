7d00d1b7202b8e19dfebc656a1fe9d977ad87fd3
Fanny Reslow <reslowf@gmail.com>
2022-07-15 18:10:56 +0200

    new approach -> left is to style a fade

--- apps/front/src/components/Item.tsx
+++ apps/front/src/components/Item.tsx
@@ -2,47 +2,15 @@ import type { ListItem } from "@prisma/client";
 import "./item.css";
 import Parser from "html-react-parser";
 
-import { useEffect, useState } from "react";
-import { isString } from "util";
-import { setEmitFlags } from "typescript";
-import { stringify } from "querystring";
-
 type Props = {
   listItem: ListItem;
 };
 
 export default function Item({ listItem }: Props) {
-  const [solo, setSolo] = useState(false);
-  const [listOfItems, setListOfItems] = useState<string[] | object[]>();
-  const [text, setText] = useState<string | undefined>();
-  const [el, setEl] = useState<JSX.Element | JSX.Element[]>();
-
-  useEffect(() => {
-    const data = listItem.desc;
-    let arr = data?.split("<p>,</p>");
-    console.log(arr);
-    console.log(listOfItems);
-    setListOfItems(arr);
-    checkTyp(listOfItems);
-  }, [listItem.desc, listOfItems]);
-
-  function checkTyp(listOfItems: (string | object)[] | undefined) {
-    let e: any;
-    if (listOfItems !== undefined) {
-      listOfItems?.forEach((item) => {
-        if (typeof item === "string") setSolo(!solo);
-        // it's a string
-        e = Parser(item);
-        setEl(e);
-      });
-    }
-    // console.log(text, "text");
-  }
-  console.log("e", el);
+  console.log(listItem.desc);
   return (
     <div className="ads-container">
-      <div className={`banner ${solo ? "solo" : "texts"}`}></div>
-      {el !== undefined && el}
+      <p dangerouslySetInnerHTML={{ __html: listItem.desc }} />
       <a href={`http://www.arbetsformedlingen.se${listItem.url}`}>to ad</a>
     </div>
   );
