e0ad7669176bea60b2b0e3b2e2fb1dd9cf2c8924
Fanny Reslow <reslowf@gmail.com>
2022-07-15 14:45:18 +0200

    deviding up to smaller text items before sadding to  front

--- apps/front/src/App.tsx
+++ apps/front/src/App.tsx
@@ -1,11 +1,12 @@
-import type { ListItem } from "@prisma/client";
-
+import type { ListItem, List } from "@prisma/client";
 import { useEffect, useState } from "react";
 import Item from "./components/Item";
+
 import "./App.css";
 
 function App() {
   const [data, setData] = useState<ListItem[]>([]);
+  const [list, setList] = useState<List>();
 
   async function getData() {
     let response = await fetch("http://localhost:3000/listitems");
@@ -13,13 +14,25 @@ function App() {
     setData(data);
   }
 
+  async function getListData() {
+    let response = await fetch("http://localhost:3000/list");
+    let data = await response.json();
+
+    setList(data[0]);
+  }
+
   useEffect(() => {
     getData();
+    getListData();
   }, []);
 
   return (
     <div className="App">
-      <h1> Front end </h1>
+      <h1> Front end ads </h1>
+      <section>
+        <p>collected from: {list && list.title}</p>
+      </section>
+      <div>{list?.updatedAt.toString().split("T")[0]}</div>
 
       <section className="container">
         {data.map((item, i) => {
--- apps/front/src/components/Item.tsx
+++ apps/front/src/components/Item.tsx
@@ -2,18 +2,33 @@ import type { ListItem } from "@prisma/client";
 import "./item.css";
 import Parser from "html-react-parser";
 
+import { useEffect, useState } from "react";
+
 type Props = {
   listItem: ListItem;
 };
 
 export default function Item({ listItem }: Props) {
-  const description = listItem.desc;
-  const data = Parser(description);
+  const [solo, setSolo] = useState(false);
+  const [listOfItems, setListOfItems] = useState<(string | object)[]>();
+
+  useEffect(() => {
+    const data = listItem.desc;
+    let arr = data?.split("<p>,</p>");
+    console.log(arr);
+    console.log(listOfItems);
+    setListOfItems(arr);
+  }, [listItem.desc, listOfItems]);
+
+  if (listOfItems !== undefined && (listOfItems as string[])) {
+    console.log(listOfItems);
+  }
+
   return (
-    <div className="bg">
-      <h1>{listItem.position}</h1>
+    <div className="ads-container">
+      s<h1>{listItem.position}</h1>
       <h2>{listItem.company}</h2>
-      <p>{data}</p>
+      <div className={`banner ${solo ? "solo" : "texts"}`}>{listOfItems}</div>
       <a href={`http://www.arbetsformedlingen.se${listItem.url}`}>to ad</a>
     </div>
   );
